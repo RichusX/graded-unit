001fd3591ff05b55de4ba1199d0c80f268ef2231
Ritvars Timermanis <me@richusx.me>
2017-03-17 01:25:01 +0000

    Added basic authorization functionality

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import web
 import updateTables
+import hashlib
+
+web.config.debug = False
 
 urls = (
     '/', 'index',
@@ -12,12 +15,19 @@ urls = (
 )
 globalVars = {'subscriber': False}
 
+app = web.application(urls, locals())
+db = web.database(host='localhost', dbn='mysql', db='sportsleague', user='sportsleague', pw='FH9Y9iaVZuBRmYoF')
+
+store = web.session.DiskStore('sessions')
+session = web.session.Session(app, store, initializer={'login': 0, 'privilege': 0})
+
+#-------------------- PAGE CLASSES --------------------
+
 class index:
     def __init__(self):
         self.render = web.template.render('templates/', base='layout', globals=globalVars)
     def GET(self):
-        listtest = ['I like potatoes', 'He likes potatoes', 'Everyone likes potatoes']
-        return self.render.index(listtest)
+        return self.render.index()
 
 class league_tables:
     def __init__(self):
@@ -38,17 +48,59 @@ class icon:
 
 class login:
     def GET(self):
-        globalVars['subscriber'] = True
-        raise web.seeother("/")
+        if logged():
+            render = create_render(session.privilege)
+            return '%s' % render.index()
+        else:
+            render = create_render(session.privilege)
+            return '%s' % render.login()
+    def POST(self):
+        username, passwd = web.input().username, web.input().passwd
+        try:
+            ident = db.select('users', where='user=$username', vars=locals())[0]
+            if hashlib.sha1(passwd).hexdigest() == ident['pass']:
+                session.login = 1
+                session.privilege = ident['privilege']
+                render = create_render(session.privilege)
+                return render.login_ok()
+            else:
+                session.login = 0
+                session.privilege = 0
+                render = create_render(session.privilege)
+                return render.login_error()
+        except:
+            session.login = 0
+            session.privilege = 0
+            render = create_render(session.privilege)
+            return render.login_error()
 
 class logout:
     def GET(self):
-        globalVars['subscriber'] = False
-        raise web.seeother("/")
+        session.login = 0
+        session.kill()
+        render = create_render(session.privilege)
+        return render.index()
+#------------------------------------------------------
+
+def logged():
+        if session.login==1:
+            return True
+        else:
+            return False
+
+def create_render(privilege):
+        if logged():
+            if privilege == 0:
+                render = web.template.render('templates/', base='layout')
+            elif privilege == 1:
+                render = web.template.render('templates/', base='layout')
+            elif privilege == 2:
+                render = web.template.render('templates/', base='layout')
+            else:
+                render = web.template.render('templates/', base='layout')
+        else:
+            render = web.template.render('templates', base='layout')
+        return render
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    web.config.degub = True
-    updateTables.getTables()
-    updateTables.mergeHTML()
     app.run()
