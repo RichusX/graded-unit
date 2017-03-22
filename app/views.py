b545ebfebeba8ee71aa4e8ee7148f3e1a8ec8262
Ritvars Timermanis <me@richusx.me>
2017-03-22 22:03:03 +0000

    Converted files for Flask

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
+@app.route('/stats')
