f6af105b9da8e0ffdacb7b9631b7111afcb27db5
Ritvars Timermanis <me@richusx.me>
2017-05-12 00:10:00 +0100

    Reconfigured server settings

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
 from app import app
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
-app.debug = True
+app.debug = False
 app.run(host='0.0.0.0', port=8080)
